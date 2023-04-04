2ccaa5b79e23e9fed5bc026070e13fbc85bbc94c
axelsvenn <mrr.axelsven@gmail.com>
2023-04-04 17:55:09 +0300

    --create MyDevice || --create DevicesAdapter

---
+++ app/src/main/java/com/example/thsensor/devices/MyDevice.java
@@ -0,0 +1,5 @@
+package com.example.thsensor.devices;
+
+public class MyDevice {
+    // need to describe this class
+}
---
+++ app/src/main/java/com/example/thsensor/ui/devices/DevicesAdapter.java
@@ -0,0 +1,35 @@
+package com.example.thsensor.ui.devices;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.thsensor.R;
+import com.example.thsensor.devices.MyDevice;
+
+import java.util.ArrayList;
+
+class DevicesAdapter extends ArrayAdapter<MyDevice> {
+
+    protected DevicesAdapter(@NonNull Context context, ArrayList<MyDevice> devices) {
+        super(context, R.layout.fragment_devices, devices);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.fragment_devices, parent, false);
+        }
+
+        // need to describe this method
+
+        return convertView;
+    }
+}
