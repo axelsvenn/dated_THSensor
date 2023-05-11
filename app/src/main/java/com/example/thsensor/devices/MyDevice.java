ce6028049727675fc1014fed84345a3e6a44c773
axelsvenn <mrr.axelsven@gmail.com>
2023-05-11 15:02:27 +0300

    --create adapter for notifications

--- app/src/main/java/com/example/thsensor/devices/MyDevice.java
+++ app/src/main/java/com/example/thsensor/devices/MyDevice.java
@@ -45,9 +45,47 @@ public class MyDevice {
         return id;
     }
 
-    protected class Notification {
-        private int deviceID;
+    public class Notification {
+        private int deviceID = id;
         private String date, time;
         private String text;
+
+        public Notification(String date, String time, String text) {
+            this.date = date;
+            this.time = time;
+            this.text = text;
+        }
+
+        public int getDeviceID() {
+            return deviceID;
+        }
+
+        public void setDeviceID(int deviceID) {
+            this.deviceID = deviceID;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
     }
 }
---
+++ app/src/main/java/com/example/thsensor/ui/notifications/NotificationsAdapter.java
@@ -0,0 +1,43 @@
+package com.example.thsensor.ui.notifications;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.thsensor.R;
+import com.example.thsensor.devices.MyDevice;
+
+import java.util.ArrayList;
+
+public class NotificationsAdapter extends ArrayAdapter<MyDevice.Notification> {
+    TextView date, time, text;
+
+    public NotificationsAdapter(@NonNull Context context, ArrayList<MyDevice.Notification> notifications) {
+        super(context, R.layout.adapter_notifications, notifications);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.adapter_notifications, parent, false);
+        }
+
+        date = convertView.findViewById(R.id.notificationDate_info);
+        time = convertView.findViewById(R.id.notificationTime_info);
+        text = convertView.findViewById(R.id.notificationText_info);
+
+        MyDevice.Notification item = getItem(position);
+        date.setText(item.getDate());
+        time.setText(item.getTime());
+        text.setText(item.getText());
+
+        return convertView;
+    }
+}
